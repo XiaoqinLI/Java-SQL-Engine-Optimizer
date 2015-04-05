93245f5c6ad664438fa555e54a98599a119d5620
XiaoqinLI <daybreaklee@utexas.edu>
2015-04-05 16:08:59 -0500

    finished Where Clause Checking

--- Assignment4/src/Interpreter.java
+++ Assignment4/src/Interpreter.java
@@ -57,9 +57,9 @@ class Interpreter {
 				System.out.println ("\t" + myFrom);
 
 				System.out.println ("WHERE clause:");
-				Expression where = parser.getWHERE ();
-				if (where != null)
-					System.out.println ("\t" + where.print ());
+				Expression myWhere = parser.getWHERE ();
+				if (myWhere != null)
+					System.out.println ("\t" + myWhere.print ());
 
 				System.out.println ("GROUPING atts:");
 				ArrayList<String> attributesGroupBy = parser.getGROUPBY();
@@ -68,7 +68,9 @@ class Interpreter {
 				}
 				
 				// semantics checking starts here.
-				semanticChecker = new SemanticCheck(res, mySelect, myFrom, attributesGroupBy);
+				
+				semanticChecker = new SemanticCheck(res, mySelect, myFrom, attributesGroupBy, myWhere);
+				
 				System.out.println("\n##### Semantic checking started. #####");
 				if(semanticChecker.checkingSQLQuery()){
 					System.out.println("\n##### Semantic checking successfully ended. #####");				
@@ -89,13 +91,19 @@ class Interpreter {
 		ArrayList<Expression> selectClause;
 		Map<String, String> fromClause;
 		ArrayList<String> groupbyClause;
+		Expression whereClause;
 		
-		SemanticCheck(Map <String, TableData> res, ArrayList<Expression> SELECT, Map<String, String> FROM, ArrayList<String> GROUPBY){		
+		static final String [] incompatibleTypesBetweenNumAndStr = {
+			"plus", "minus", "times", "divided by", "equals", "greater than", "less than"
+		};
+		
+		SemanticCheck(Map <String, TableData> res, ArrayList<Expression> SELECT, Map<String, String> FROM, ArrayList<String> GROUPBY, Expression WHERE){		
 			// TODO Auto-generated constructor stub
 			this.dataMap = res;
 			this.selectClause =  SELECT;
 			this.fromClause = FROM;
 			this.groupbyClause = GROUPBY;
+			this.whereClause = WHERE;
 		}
 
 		public boolean checkingSQLQuery(){
@@ -103,10 +111,10 @@ class Interpreter {
 			// checking the From clause of the query
 			System.out.println("-----Checking the 'From' clause---------------------------------------------");
 			if(!isValidFromClause()){
-				System.out.println("Invalid syntax Found in 'From' clause");
+				System.out.println("Invalid syntax Found the in 'From' clause");
 				return false;
 			}else{
-				System.out.println("'From' clause is validated");
+				System.out.println("The 'From' clause is validated");
 			}
 			System.out.println("----------------------------------------------------------------------------\n");
 			
@@ -114,26 +122,38 @@ class Interpreter {
 			// Attribute in corresponding table in the SELECT Clause
 			System.out.println("-----Checking the Alias and Attribute of Identifiers in 'Select' clause-----");
 			if(!isValidIdentifierSelectClause()){
-        		System.out.println("Invalid syntax found in 'SELECT' clause");
+        		System.out.println("Invalid syntax found in the 'Select' clause");
         		return false;
         	}else{
-        		System.out.println("Alias and Attribute in 'Select' clause are all validated");
+        		System.out.println("Alias and Attribute in the 'Select' clause are all validated");
         	}
 	        System.out.println("----------------------------------------------------------------------------\n");
 	        
-	        // Checking Group Clause and corresponding Syntax in Select Clause
+	        // Checking the Group Clause and corresponding Syntax in Select Clause
 	        System.out.println("-----Checking the Group Clause----------------------------------------------");
-	        if((groupbyClause.size() > 0) && !isValidGroupByClause()){
-	        	System.out.println("Invalid syntax found in 'Group By' or its corresponding 'Select' clause");
+	        if( (groupbyClause.size() > 0) && !isValidGroupByClause() ){
+	        	System.out.println("Invalid syntax found in the 'Group By' clause or its corresponding 'Select' clause");
 	        	return false;
 	        }
 	        else if(groupbyClause.size() == 0){
 	        	System.out.println("No 'Group By' clause exists");
 	        }else{
-	        	System.out.println("'Group By' clause is validated");
+	        	System.out.println("The 'Group By' clause is validated");
 	        }
 	        System.out.println("----------------------------------------------------------------------------\n");
 	       
+	        // Checking the Where Clause and corresponding types' mismatches.
+	        System.out.println("-----Checking the Where Clause----------------------------------------------");
+	        if( (whereClause != null) && ( !(isValidWhereClause(whereClause).isTypeValid()) )){
+	        	System.out.println("Invalid syntax found in the 'Where' clause");
+	        	return false;
+	        }
+	        else if (whereClause != null){
+	        	System.out.println("No 'Where' clause exists");
+	        }else{
+	        	System.out.println("The 'Where' clause is validated");
+	        }
+	        
 			return true;
 		}
 
@@ -216,20 +236,95 @@ class Interpreter {
 				
 			// checking if expression in Select Clause is valid to the Group By Clause
 			for(Expression selectEle : selectClause){
-				System.out.println(selectEle.getType());
 				if (isUnaryOperation(selectEle.getType())){
 					// Unary types are allowed in the select clause when GroupBy exists
 				}	
 				else if (!(selectEle.getType().equals("identifier") && groupbyClause.contains( selectEle.getValue()))){
 					System.out.println("Error: Expression "+ selectEle.print() +" expression is not allowed in the select clause when GroupBy exists");
 					return false;	
-				}
-				
+				}	
 			}
 			return true;
 		}
 		
+		ExpressionIsTypeValid isValidWhereClause(Expression WHERE) {
+			
+			// first of all, check the CNF(and/or) recursively:
+			if(WHERE.getType().equals("or") || WHERE.getType().equals("and")){
+				ExpressionIsTypeValid leftSubExp = isValidWhereClause(WHERE.getLeftSubexpression());
+				ExpressionIsTypeValid rightSubExp = isValidWhereClause(WHERE.getRightSubexpression());
+
+				if(leftSubExp.isTypeValid() && rightSubExp.isTypeValid()){
+					return new ExpressionIsTypeValid(0, true);
+				}else{
+					return new ExpressionIsTypeValid(0, false);
+				}				  
+			}
+			
+			// secondly, check sum, avg and other unary operation
+			if(isUnaryOperation(WHERE.getType())){	
+				ExpressionIsTypeValid leftSubExpression = isValidWhereClause(WHERE.getLeftSubexpression());
+				if(!leftSubExpression.isTypeValid())
+					System.out.println("ERROR: Incompatible unaryTypes found: " + WHERE.print());			
+
+				if(WHERE.getType().equals("not"))
+					return leftSubExpression;
+				
+				// string type can't be in unary operation
+				else if (leftSubExpression.getExpType() == 1 ){
+					System.out.println("ERROR: Incompatible unaryTypes found: " + WHERE.print());
+					return new ExpressionIsTypeValid(-1, false);
+				}
+				
+				else
+					return leftSubExpression;
+			  }
+			
+			// then check all other BinaryOperations
+			String exppresionType = WHERE.getType();
+			
+			if(isBinaryOperation(exppresionType)){
+				ExpressionIsTypeValid leftSubExpression = isValidWhereClause(WHERE.getLeftSubexpression());
+				ExpressionIsTypeValid rightSubExpression = isValidWhereClause(WHERE.getRightSubexpression());
+				 
+				if((leftSubExpression != null) && (rightSubExpression != null)){
+					ExpressionIsTypeValid expressionValid = checkBinaryOperation(leftSubExpression, rightSubExpression, exppresionType);	
+					if(!expressionValid.isTypeValid())
+						System.out.println("ERROR: Incompatible bianry operation found in: " + WHERE.print());
+
+					return expressionValid;		  
+				}	
+			  }	  			
+			
+			//check identifiers
+			String expType;
+			if(WHERE.getType().equals("identifier")){
+				String expressionString = WHERE.getValue();
+				expType = getExpressionAtributeType(expressionString);
+
+				if(expType == null){
+					// System.out.println("Error: "+exp.getValue() +"  is not the valid attribute of the table");
+					return (new ExpressionIsTypeValid(-1, false));
+				}
+				else if (expType.equals("Int") || expType.equals("Float"))
+					return (new ExpressionIsTypeValid(2, true));	  
+				else if(expType.equals("Str")){
+					return (new ExpressionIsTypeValid(1, true));	
+				}
+
+			}
+			
+			// check other valueTypes
+			if (WHERE.getType().equals("literal float") || WHERE.getType().equals("literal int"))
+				return (new ExpressionIsTypeValid(2, true));
+			else if(WHERE.getType().equals("literal string"))
+				return (new ExpressionIsTypeValid(1, true));
+
+			// unknown invalid types
+			return null; //new ExpressionIsTypeValid(-1, false);
+		}
 		
+		// helper functions:
 		private boolean isUnaryOperation(String exppressionType) {
 			for (String UnaryTppe : Expression.unaryTypes) {
 				if(UnaryTppe.equals(exppressionType))
@@ -245,8 +340,84 @@ class Interpreter {
 			}
 			return false;
 		}
-
 		
+		private String getExpressionAtributeType(String expression){
+		  	String attributeType;
+			String alias = expression.substring(0, expression.indexOf("."));
+			String attributeName = expression.substring(expression.indexOf(".")+1);
+			String currentTableName = fromClause.get(alias);
+			
+			if(currentTableName == null){
+				System.out.println("Error: Alias '"+ alias +"' does not exist in any table selected the FROM clause");
+				return null;
+			}
+			
+			Map<String, AttInfo> allAttributesInfo = dataMap.get(currentTableName).getAttributes();
+			
+			if (allAttributesInfo == null){
+				System.out.println("Error: Table '"+ currentTableName +"' does not exist in the CATALOGUE");
+				return null;
+			}
+			
+			if(!(allAttributesInfo.containsKey(attributeName))){
+				System.out.println("Error: Attribute '"+ attributeName +"' does not exist in the TABLE: "+ currentTableName);
+				return null;
+			}
+			else
+				attributeType = allAttributesInfo.get(attributeName).getDataType();
+
+			return attributeType;
+
+		}
+		
+		private ExpressionIsTypeValid checkBinaryOperation(ExpressionIsTypeValid left, ExpressionIsTypeValid right, String expressionType){
+
+			if(left.isTypeValid() && right.isTypeValid()){			  
+				// expression type is int or float
+				if(left.getExpType() == 2){ 
+					if(right.getExpType() == 1){ // str
+						System.out.println("Invalid Binary operation between Number and String");
+						return (new ExpressionIsTypeValid(-1, false));
+//						for(String incompatibilityTypes : incompatibleTypesBetweenNumAndStr){
+//							if(expressionType.equals(incompatibilityTypes)){
+//								return (new ExpressionIsTypeValid(-1, false));
+//							}				
+//						}
+					}
+					return (new ExpressionIsTypeValid(2, true));
+				}
+				
+				// expression type is str	
+				else if(left.getExpType() == 1){
+					if (right.getExpType() == 1){ // str
+						if (expressionType.equals("minus") || expressionType.equals("times") || expressionType.equals("divided by")){
+							System.out.println("Invalid Binary operation between Strings.");
+							return (new ExpressionIsTypeValid(-1, false));
+						}else{
+							return (new ExpressionIsTypeValid(1, true));
+						}
+					}
+					else if (right.getExpType() == 2){
+						System.out.println("Invalid Binary operation between String and Number");
+						return (new ExpressionIsTypeValid(-1, false));
+//						for(String incompatibilityTypes : incompatibleTypesBetweenNumAndStr){
+//							if(expressionType.equals(incompatibilityTypes)){
+//								return (new ExpressionIsTypeValid(-1, false));
+//							}				
+//						}
+					}
+					else{ // other type
+						return (new ExpressionIsTypeValid(1, true));
+					}					
+				}else{ // unknown type
+					return (new ExpressionIsTypeValid(-1, false));	
+				}				
+			}
+			else{ // invalid
+				return (new ExpressionIsTypeValid(-1, false));		  
+			}
+		}
 	
-	}
+	}// end of inner semanticCheck class
+	
 }
